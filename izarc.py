566b2352119dec00b1bb1c8bdce44966a23e576a
Igor Lvovsky <igorl@infinidat.com>
2013-11-28 15:54:33 +0200

    Add more statistics
    - Use -x to extended ARC statistics
    - Use -l to L2ARC statistics

--- izarc.py
+++ izarc.py
@@ -71,10 +71,19 @@ METRIC_NAMES = {'hps': 'hit/s', 'mps': 'miss/s',
     'imsbs': 'imssb/s', 'imscs': 'imssc/s',
     'incbs': 'incb/s', 'inccs': 'incc/s',
     'icbs': 'icb/s', 'iccs': 'icc/s',
+    'mrus': 'dltsize', 'mruh': 'hits/s', 'mrudevc': 'devict/s', 'mrumevc': 'mevict/s',
+    'mfus': 'dltsize', 'mfuh': 'hits/s', 'mfudevc': 'devict/s', 'mfumevc': 'mevict/s',
+    'gmrus': 'dltsize', 'gmruh': 'hits/s', 'gmrudevc': 'devict/s', 'gmrumevc': 'mevict/s',
+    'gmfus': 'dltsize', 'gmfuh': 'hits/s', 'gmfudevc': 'devict/s', 'gmfumevc': 'mevict/s',
+    'l2hps': 'hit/s', 'l2mps': 'miss/s',
+    'l2size': 'dltsize', 'l2hdrsize': 'dlthdrsize',
+    'l2read': 'read/s', 'l2write': 'write/s', 'l2sent': 'sent/s',
+    'l2whm': 'writehdrm/s', 'l2fow': 'freeonwrite/s',
     }
 
 HEADER_NAMES = {'total': '  TOTAL', 'demand': 'DEMAND', 'prefetch': 'PREFETCH',
-    'arc': 'ARC SIZE', 'transactions': 'TRANSACTIONS', 'copy': 'DATA COPY'}
+    'arc': 'ARC SIZE', 'transactions': 'TRANSACTIONS', 'copy': 'DATA COPY',
+    'mru': 'MRU', 'mfu': 'MFU', 'gmru': 'GHOST MRU', 'gmfu': 'GHOST MFU'}
 
 ARC_HEADER = '{total:^16}{demand:^32}{prefetch:^32}{arc:^16}'
 OUT_TOTAL = '{hps:>8}{mps:>8}'
@@ -83,11 +92,24 @@ OUT_PREFETCH = '{pdhps:>8}{pdmps:>8}{pmdhps:>8}{pmdmps:>8}'
 OUT_ARC = '{c!s:>8}{p!s:>8}'
 ARC_FORMAT = OUT_TOTAL + OUT_DEMAND + OUT_PREFETCH + OUT_ARC
 
+EXTENDEDARC_HEADER = '{mru:^36}{mfu:^36}{gmru:^36}{gmfu:^36}'
+OUT_MRU = '{mrus!s:>8}{mruh:>8}{mrudevc:>10}{mrumevc:>10}'
+OUT_MFU = '{mfus!s:>8}{mfuh:>8}{mfudevc:>10}{mfumevc:>10}'
+OUT_GMRU = '{gmrus!s:>8}{gmruh:>8}{gmrudevc:>10}{gmrumevc:>10}'
+OUT_GMFU = '{gmfus!s:>8}{gmfuh:>8}{gmfudevc:>10}{gmfumevc:>10}'
+EXTENDEDARC_FORMAT = OUT_MRU + OUT_MFU + OUT_GMRU + OUT_GMFU
+
+L2ARC_SIZE = '{l2size!s:>14}{l2hdrsize!s:>14}'
+L2ARC_IO = '{l2hps:>8}{l2mps:>8}{l2read:>8}{l2write:>8}{l2sent:>8}'
+L2ARC_MISC = '{l2whm:>14}{l2fow:>14}'
+L2ARC_FORMAT = L2ARC_SIZE + L2ARC_IO + L2ARC_MISC
+
 ZIL_HEADER = '{total:^20}{transactions:^40}{copy:^32}'
 ZIL_COMMITS = '{cps:>10}{wps:>10}'
 ZIL_ITX = '{itxs:>8}{imnbs!s:>8}{imncs:>8}{imsbs!s:>8}{imscs:>8}'
 ZIL_COPIES = '{incbs!s:>8}{inccs:>8}{icbs!s:>8}{iccs:>8}'
 ZIL_FORMAT = ZIL_COMMITS + ZIL_ITX + ZIL_COPIES
+
 
 def humanize(number):
     number = int(number)
@@ -128,7 +150,7 @@ class Integer(long):
         return humanize(self.numerator)
 
 
-class kstat():
+class kstat(object):
     KSTATBASE = '/proc/spl/kstat'
     # kstat types
     KSTAT_TYPE_RAW = 0
@@ -197,7 +219,7 @@ class kstat():
 
 class arcstats(kstat):
     def __init__(self):
-        kstat.__init__(self, 'zfs', 'arcstats')
+        super(arcstats, self).__init__('zfs', 'arcstats')
         self._arcstats = dict()
         for name in self._kstat:
             self._arcstats[name] = Integer(self._kstat[name])
@@ -211,7 +233,7 @@ class arcstats(kstat):
                 self._arcstats[name] = Integer(value)
 
     def __sub__(self, other):
-        diff = arcstats()
+        diff = self.__class__()
         diff._snaptime = self._snaptime - other._snaptime
         for item in self._arcstats:
             diff._arcstats[item] = Integer(self._arcstats[item] - other._arcstats[item])
@@ -271,9 +293,74 @@ class arcstats(kstat):
         return ARC_FORMAT.format(**raw)
 
 
+class extendarc(arcstats):
+    def compute(self):
+        delta = self._snaptime / NANOSEC
+        raw = self._arcstats.copy()
+        raw['mrus'] = self._arcstats['mru_size']
+        raw['mruh'] = self._arcstats['mru_hits'] / delta
+        raw['mrudevc'] = self._arcstats['mru_evict_data'] / delta
+        raw['mrumevc'] = self._arcstats['mru_evict_metadata'] / delta
+        raw['mfus'] = self._arcstats['mfu_size']
+        raw['mfuh'] = self._arcstats['mfu_hits'] / delta
+        raw['mfudevc'] = self._arcstats['mfu_evict_data'] / delta
+        raw['mfumevc'] = self._arcstats['mfu_evict_metadata'] / delta
+        raw['gmrus'] = self._arcstats['mru_ghost_size']
+        raw['gmruh'] = self._arcstats['mru_ghost_hits'] / delta
+        raw['gmrudevc'] = self._arcstats['mru_ghost_evict_data'] / delta
+        raw['gmrumevc'] = self._arcstats['mru_ghost_evict_metadata'] / delta
+        raw['gmfus'] = self._arcstats['mfu_ghost_size']
+        raw['gmfuh'] = self._arcstats['mfu_ghost_hits'] / delta
+        raw['gmfudevc'] = self._arcstats['mfu_ghost_evict_data'] / delta
+        raw['gmfumevc'] = self._arcstats['mfu_ghost_evict_metadata'] / delta
+        return raw
+
+    def headers(self):
+        header = '\n'
+        header += EXTENDEDARC_HEADER.format(**HEADER_NAMES)
+        header += '\n'
+        header += EXTENDEDARC_FORMAT.format(**METRIC_NAMES)
+        return header
+
+    def data(self):
+        raw = self.compute()
+        return EXTENDEDARC_FORMAT.format(**raw)
+
+
+class l2arc(arcstats):
+    def compute(self):
+        delta = self._snaptime / NANOSEC
+        raw = self._arcstats.copy()
+        raw['l2size'] = self._arcstats['l2_size']
+        # L2ARC headers (ARC overhead)
+        raw['l2hdrsize'] = self._arcstats['l2_hdr_size']
+        raw['l2hps'] = self._arcstats['l2_hits'] / delta
+        raw['l2mps'] = self._arcstats['l2_misses'] / delta
+        raw['l2read'] = self._arcstats['l2_read_bytes'] / delta
+        raw['l2write'] = self._arcstats['l2_write_bytes'] / delta
+        raw['l2sent'] = self._arcstats['l2_writes_sent'] / delta
+        # This buffer misses out.  It may be in a stage
+        # of eviction.  Its ARC_L2_WRITING flag will be
+        # left set, denying reads to this buffer.
+        raw['l2whm'] = self._arcstats['l2_writes_hdr_miss'] / delta
+        # Free the arc data buffer.  If it is an l2arc write in progress,
+        # the buffer is placed on l2arc_free_on_write to be freed later.
+        raw['l2fow'] = self._arcstats['l2_free_on_write'] / delta
+        return raw
+
+    def headers(self):
+        header = '\n'
+        header += L2ARC_FORMAT.format(**METRIC_NAMES)
+        return header
+
+    def data(self):
+        raw = self.compute()
+        return L2ARC_FORMAT.format(**raw)
+
+
 class zil(kstat):
     def __init__(self):
-        kstat.__init__(self, 'zfs', 'zil')
+        super(zil, self).__init__('zfs', 'zil')
 
     def summary(self):
         return self.summary()
@@ -342,6 +429,10 @@ def execute(args):
         if args.debug:
             print zil()
         cycle(zil, args.interval, args.count)
+    elif args.l2arc:
+        cycle(l2arc, args.interval, args.count)
+    elif args.extend:
+        cycle(extendarc, args.interval, args.count)
     else:
         cycle(arcstats, args.interval, args.count)
 
@@ -358,6 +449,10 @@ def main():
         help='print ARC summary information', action='store_true')
     parser.add_argument('-z', '--zil',
         help='print ZIL statistics', action='store_true')
+    parser.add_argument('-x', '--extend',
+        help='print extended ARC statistics', action='store_true')
+    parser.add_argument('-l', '--l2arc',
+        help='print L2ARC statistics', action='store_true')
     parser.add_argument('interval',
         help='seconds between probes', type=int, nargs='?', default=1)
     parser.add_argument('count',
